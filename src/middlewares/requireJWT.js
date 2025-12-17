f5a976253c88ee042f58b8c73f123c3cb3b605fb
radiumSodium <si.radiumsodium@gmail.com>
2025-12-17 15:33:23 +0600

    design: create service page redesigned

--- src/middlewares/requireJWT.js
+++ src/middlewares/requireJWT.js
@@ -1,19 +1,3 @@
-// const jwt = require("jsonwebtoken");
-
-// module.exports = function requireJWT(req, res, next) {
-//   try {
-//     const auth = req.headers.authorization || "";
-//     const token = auth.startsWith("Bearer ") ? auth.slice(7) : null;
-//     if (!token) return res.status(401).json({ ok: false, message: "No JWT" });
-
-//     const payload = jwt.verify(token, process.env.JWT_SECRET);
-//     req.user = payload; // { _id, uid, email, role }
-//     next();
-//   } catch (e) {
-//     return res.status(401).json({ ok: false, message: "Invalid JWT" });
-//   }
-// };
-
 const jwt = require("jsonwebtoken");
 const user = require("../models/user");
 
